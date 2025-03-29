d7809f43f5aae0b86be153b2841517e565262914
B.Billion <buhebillion@gmail.com>
2025-03-29 13:26:52 +0200

    we have to make a makefile

---
+++ intFahrToCelcGCCv1.asm
@@ -0,0 +1,6 @@
+;DESCRIPTION                  : This programm prints the integer values of the
+;                             : Fahrenheit-Celcius table for the range 0 bis 300 in Fahrenheit.
+;
+;ARCHITECTURE                 : X86-64
+;CPU                          : Intel® Core™2 Duo CPU T6570 @ 2.10GHz × 2
+;GCC                          : 9.4.0
